7abcdab631dc75fcf92daeb0f561a4e4094f082d
balevin11 <kcancelol23fpcoruna.afundacion.org>
2024-10-01 14:41:03 +0200

    Ejercicio 1 completado

---
+++ sprint0phython/operaciones.py
@@ -0,0 +1,11 @@
+def suma (n1,n2):
+    return n1+n2
+def resta (n1,n2):
+    return n1-n2
+def multiplica (n1,n2):
+    return n1*n2
+def division (n1,n2):
+   if n2 == 0:
+       return 0
+   else:
+       return n1/n2
