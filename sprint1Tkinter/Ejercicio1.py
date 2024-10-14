d83fde6de64fc8f33213d0c0fd813d6c3e9839e3
balevin11 <kcancelol23fpcoruna.afundacion.org>
2024-10-14 16:35:05 +0200

    Tarea completada 1

---
+++ sprint1Tkinter/Ejercicio1.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+
+#Creamos la funcion para cambiar el texto
+def cambio_texto():
+    label3.config(text="Texto 2")
+
+#crear la ventana principal
+root = tk.Tk()
+root.title("DI Sprint1")
+root.geometry("300x200")
+
+#crear los textos
+label1 = tk.Label(root, text="Bienvenido")
+label1.pack()
+label2 = tk.Label(root, text="Kevin Cancelo")
+label2.pack()
+label3 = tk.Label(root, text="Texto 1")
+label3.pack()
+
+#crear el boton que invoque la funcion cambio_texto
+button = tk.Button(root,text="      ", command = cambio_texto)
+button.pack()
+
+#ejecutar el bucle principal
+root.mainloop()
+
