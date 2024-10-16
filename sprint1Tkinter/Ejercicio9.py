1355c422ec02043901bd414f9b940a7a79b5ebca
balevin11 <kcancelol23fpcoruna.afundacion.org>
2024-10-16 09:58:02 +0200

    Tarea completada 9

---
+++ sprint1Tkinter/Ejercicio9.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def salir():
+    root.quit()
+
+def nueva_ventana():
+    messagebox.showinfo("Nuevo", "Abrir una nueva ventana")
+
+def mensaje_ayuda():
+    messagebox.showinfo("Ayuda","Ejemplo de ayuda")
+
+#crear la ventana principal
+root = tk.Tk()
+root.title('Ejercicio 3')
+root.geometry('300x200')
+
+#crear el menu principal
+main_menu = tk.Menu(root)
+root.config(menu=main_menu)
+
+#crear submenú abrir
+archive_menu = tk.Menu(main_menu, tearoff=0)
+main_menu.add_cascade(label='Archivo', menu=archive_menu)
+(archive_menu.add_command(label='Abrir', command=nueva_ventana))
+archive_menu.add_separator()
+archive_menu.add_command(label='Salir', command=salir)
+
+#crear submenú ayuda
+help_menu = tk.Menu(main_menu, tearoff=0)
+main_menu.add_cascade(label='Archivo', menu=help_menu)
+help_menu.add_command(label='Acerca de', command=mensaje_ayuda)
+
+#ejecutar el bucle principal
+root.mainloop()
