4a95da391bd4639742a4ccdb1271e34cbf4feaaa
kevin cancelo <kcancelol23fpcoruna.afundacion.org>
2024-11-22 10:44:26 +0100

    Ejercicio 2 completado

--- sprint3android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
+++ sprint3android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.catalogactivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button button = findViewById(R.id.detail);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                context.startActivity(intent);
+            }
         });
     }
 }
